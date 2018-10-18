3911f2759a6e460c2f284f0dcc9f7df10fccef83
Arpad Perenyi <arpad.perenyi@gmail.com>
2018-10-18 16:02:35 +0200

    Build module with const function

--- ocaml/generator.ml
+++ ocaml/generator.ml
@@ -1,5 +1,7 @@
 #load "wasm.cmo";;
 
+open Wasm
+
 (* type 'a phrase = {at : region; it : 'a} *)
 (*
 type module_' = {
@@ -14,9 +16,59 @@ type module_' = {
   imports : import list;
   exports : export list;
 }
+
+type func = func' Source.phrase
+and func' =
+{
+  ftype : var;
+  locals : value_type list;
+  body : instr list;
+}
 *)
 
-let empty = Wasm.Ast.empty_module;;
-let empty_module = {Wasm.Source.at = Wasm.Source.no_region; Wasm.Source.it = empty};;
-let arrange_m = Wasm.Arrange.module_ empty_module;;
-Wasm.Sexpr.print 0 arrange_m;;
+let string_to_name s =
+  let rec exp i l =
+    if i < 0 then l else exp (i - 1) (Char.code(s.[i]) :: l) in
+  exp (String.length s - 1) []
+;;
+
+let as_phrase x = {Source.at = Source.no_region; Source.it = x}
+;;
+
+let empty_f = 
+{
+  Ast.ftype = as_phrase 0l;
+  Ast.locals = [];
+  Ast.body = [as_phrase (Ast.Const (as_phrase (Values.I32 42l)));];
+}
+
+let get_module types funcs = {
+  Ast.types = types;
+  Ast.globals = [];
+  Ast.tables = [];
+  Ast.memories = [];
+  Ast.funcs = funcs;
+  Ast.start = None;
+  Ast.elems  = [];
+  Ast.data = [];
+  Ast.imports = [];
+  Ast.exports = [
+    as_phrase ({
+      Ast.name = string_to_name "aexp";
+      Ast.edesc = as_phrase (Ast.FuncExport (as_phrase 0l));
+    })
+  ];
+}
+;;
+
+let types_ = [ 
+  as_phrase (Types.FuncType ([], [Types.I32Type]));
+  as_phrase (Types.FuncType ([], [Types.I64Type]));
+  as_phrase (Types.FuncType ([], [Types.F32Type]));
+  as_phrase (Types.FuncType ([], [Types.F64Type]))
+];;
+
+let empty = get_module types_ [as_phrase (empty_f)] in
+  let empty_module = as_phrase (empty) in
+    let arrange_m = Arrange.module_ empty_module in
+      Sexpr.print 80 arrange_m;;
